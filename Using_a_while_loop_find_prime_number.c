8bf065a7f43221482dbcd0e1bfd019666e524065
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-22 22:00:30 +0530

    using a while loop find prime number

---
+++ Using_a_while_loop_find_prime_number.c
@@ -0,0 +1,26 @@
+#include<stdio.h>
+int main(){
+    int n;
+    int i=2;
+    printf("enter the number:");
+    scanf("%d",&n);
+   int prime=0;
+   if(n==1 || n==0){
+    prime=1;
+   }
+   while (i<n)
+   {
+    if(n%i==0){
+        prime=1;
+    }
+    i++;
+   }
+   if(prime){
+    printf("%d is not prime number",n);
+   }
+   else{
+    printf("%d is prime number",n);
+   }
+   
+
+}

4733795b179d87b4429d1c73799f3ddd6f2dd056
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-23 20:37:02 +0530

    syntax of function

---
+++ syntax_of_function.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+void display();    //function prototype
+int main(){     
+    int a;      //variable declaration
+    display();   //function call
+    return 0;   //return statement
+}
+void display(){   //function defination
+    printf("hi i am display\n");   //printing the message
+}

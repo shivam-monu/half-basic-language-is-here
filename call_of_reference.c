4384047169182750554cc476872acf0b68b47e3b
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-26 13:49:39 +0530

    call by referce

---
+++ call_of_reference.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+int sum(int*,int*);
+
+//sum should change the value
+int sum(int*a,int*b){
+    *a=6;
+    return(*a+*b);
+
+}
+int main(){
+    int x=1,y=6;
+    printf("the sum of 1 and 6 is %d\n",sum(&x,&y));
+    printf("the value of x is %d",x);
+}

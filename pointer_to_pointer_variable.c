b5b1a4e3504bd02b8d3ce9675b48d4226f9ebad0
Shivam Kumar Yadav <sk4262@srmist.edu.in>
2025-08-29 23:32:58 +0530

    using a pointer find the varible to varible address and value

---
+++ pointer_to_pointer_variable.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+int main(){
+    int i=2;
+    int* ptr1=&i;
+    int** ptr2=&ptr1;
+    printf("the address of i is %u\n",ptr1);
+    printf("the value of i is %d\n",*ptr1);
+    printf("the value of i is %d\n",**ptr2);
+}
